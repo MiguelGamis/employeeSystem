743434bc789fb4b7436e1d9b54305ba02cbe1d26
mglgms <mglgms@cs.ubc.ca>
2015-07-11 18:46:00 -0700

    testing edits

--- indexa.php
+++ indexa.php
@@ -62,7 +62,7 @@ try
 					$content .= "<table>";
 					$content .= "<td width=300px><h3>Entry request for ".$request->firstName." ".$request->lastName."</h3></td>";
 					$content .= "<td>Submitted:</td><td width=390px id='submissionDate$requestID'/></td>\n";
-					$content .= "<td><a title='Edit' href='entryform.php?action=edit&requestid=$requestID'><div class='icon edit'></div></a><a title='Cancel' href='".get_redirect_url("deleterequest.php?requestid=".$requestID."&employeeid=".$request->employeeID)."'><div class='icon delete'></div></a></td>\n";
+					$content .= "<td><a href='entryform.php?action=edit&requestid=$requestID'><div class='icon edit'></div></a><a title='Cancel' href='".get_redirect_url("deleterequest.php?requestid=".$requestID."&employeeid=".$request->employeeID)."'><div class='icon delete'></div></a></td>\n";
 					$content .= "</table>";
 					$content .= "<table cellpadding='20'width='100%'><col width='1*'><col width='1*'><col width='1*'><tr>
 					<td><h4><div class='bigicon admin'></div>Administration Tasks</h4>$adminmessage</div><button name='adminexpand' onclick='expand(\"admin\", $requestID)'><div class='icon expand'></div></button><br></td>
